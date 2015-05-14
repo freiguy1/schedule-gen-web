76b7f9c4f762f7dd46d221100e5f476eed46360f
Ethan Frei <ethan.frei@gmail.com>
2015-05-14 21:27:41 +0000

    updates for redirecting

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,14 @@ fn main() {
     let mut server = Nickel::new();
     let mut router = Nickel::router();
     api_controller::ApiController::initialize(&mut router);
-    router.get("**", middleware!("Hello, World!"));
+    router.get("**", middleware! { |request, mut response| 
+        response.set_status(nickel::status::StatusCode::TemporaryRedirect);
+        response.origin.headers_mut().set_raw("location", vec![b"https://github.com/freiguy1/schedule-gen-web".to_vec()]);
+        return response.send("");
+    });
+    
+    /*(nickel::status::StatusCode::TemporaryRedirect, "Redirecting to github", vec!["https://github.com/freiguy1/schedule-gen-web"])));
+     */
     server.utilize(router);
     server.listen("0.0.0.0:3000");
 }
