ecd6676691cb08b506c4ca22640efa76c3b1bc6b
Ethan Frei <ethan.frei@gmail.com>
2015-05-22 19:57:21 +0000

    adding index page/template/javascript

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,35 @@ mod contracts;
 mod api_controller;
 
 use hyper::header::Location;
-use nickel::Nickel;
+use nickel::{ StaticFilesHandler, Nickel };
 use nickel::router::http_router::HttpRouter;
+use std::collections::HashMap;
+
+const GITHUB_URL: &'static str = "https://github.com/freiguy1/schedule-gen-web";
 
 
 fn main() {
     let mut server = Nickel::new();
+    
+    // Enable Router
     let mut router = Nickel::router();
     api_controller::ApiController::initialize(&mut router);
-    router.get("**", middleware! { |request, mut response| 
-        response.origin.headers_mut().set(Location("https://github.com/freiguy1/schedule-gen-web".into()));
+    router.get("/github", middleware! { |request, mut response| 
+        response.origin.headers_mut().set(Location(GITHUB_URL.into()));
         (nickel::status::StatusCode::TemporaryRedirect, "")
     });
-    
+
+    router.get("/", middleware! { |_req, res|
+        let data = HashMap::<&str, &str>::new();
+        return res.render("templates/index.tpl", &data);
+    });
+
+    // router.get("/assets/*", StaticFilesHandler::new("assets/"));
+
+    server.utilize(StaticFilesHandler::new("public/"));
+
     server.utilize(router);
+
     server.listen("0.0.0.0:6767");
 }
 
---
+++ templates/index.tpl
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/0.8.3/angular-material.min.css">
+        <title>
+            League Schedule Generator
+        </title>
+    </head>
+    <body>
+        <h3>Hello world!</h3>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-aria.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angular_material/0.8.3/angular-material.min.js"></script>
+        <script type="text/javascript" src="/assets/js.js"></script>
+    </body>
+</html>
