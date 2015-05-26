bffd28bbdaf3056db60ae49bfc75bc0c287d205a
Ethan Frei <ethan.frei@gmail.com>
2015-05-26 22:19:06 +0000

    adding locations and remove buttons

--- public/assets/js.js
+++ public/assets/js.js
@@ -1,7 +1,51 @@
-var app = angular.module('ScheduleGenApp', ['ngMaterial']);
-
+var app = angular.module('ScheduleGenApp', ['ngMaterial', 'ngMdIcons']);
 
 app.controller('AppCtrl', function ($scope) {
-    $scope.teams = [ "Team 1", "Team 2" ];
-    $scope.addTeam = function() { $scope.teams.push(''); };
+    // TEAMS
+
+    $scope.teams = [ 
+        { name: "Team 1", id: 0 },
+        { name: "Team 2", id: 1 }];
+
+    $scope.removeTeam = function(id) {
+        var oldTeams = $scope.teams;
+        $scope.teams = [];
+        angular.forEach(oldTeams, function(team) {
+            if(team.id != id) {
+                $scope.teams.push(team);
+            }
+        });
+    };
+
+    $scope.addTeam = function() { 
+        $scope.teams.push({ 
+            name: '',
+            id: $scope.teams[$scope.teams.length - 1].id + 1 
+        });
+    };
+
+
+    // LOCATIONS
+    $scope.locations = [ 
+        { name: "Baseball Field 1", id: 0 },
+        { name: "Baseball Field 2", id: 1 }];
+
+    $scope.removeLocation = function(id) {
+        var oldLocations = $scope.locations;
+        $scope.locations = [];
+        angular.forEach(oldLocations, function(location) {
+            if(location.id != id) {
+                $scope.locations.push(location);
+            }
+        });
+    };
+
+    $scope.addLocation = function() { 
+        $scope.locations.push({ 
+            name: '',
+            id: $scope.locations[$scope.locations.length - 1].id + 1 
+        });
+    };
+
+
 });
