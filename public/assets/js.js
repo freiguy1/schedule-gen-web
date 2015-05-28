4557dee39668151f4fa04a125a0157bebcf1a498
Ethan Frei <ethan.frei@gmail.com>
2015-05-28 14:48:12 +0000

    moving all the things to an anonymous self called function in javascript

--- public/assets/js.js
+++ public/assets/js.js
@@ -1,67 +1,68 @@
-var app = angular.module('ScheduleGenApp', ['ngMaterial', 'ngMdIcons']);
+(function(){
+    var app = angular.module('ScheduleGenApp', ['ngMaterial', 'ngMdIcons']);
 
-app.controller('AppCtrl', function ($scope) {
-    // TEAMS
+    app.controller('AppCtrl', function ($scope) {
+        // TEAMS
+        $scope.teams = [ 
+            { name: "Team 1", id: 0 },
+            { name: "Team 2", id: 1 }];
 
-    $scope.teams = [ 
-        { name: "Team 1", id: 0 },
-        { name: "Team 2", id: 1 }];
+        $scope.removeTeam = function(id) {
+            var oldTeams = $scope.teams;
+            $scope.teams = [];
+            angular.forEach(oldTeams, function(team) {
+                if(team.id != id) {
+                    $scope.teams.push(team);
+                }
+            });
+        };
 
-    $scope.removeTeam = function(id) {
-        var oldTeams = $scope.teams;
-        $scope.teams = [];
-        angular.forEach(oldTeams, function(team) {
-            if(team.id != id) {
-                $scope.teams.push(team);
+        $scope.addTeam = function() { 
+            var newId = 0;
+
+            if ($scope.teams.length != 0) {
+                newId = $scope.teams[$scope.teams.length - 1].id + 1;
             }
-        });
-    };
 
-    $scope.addTeam = function() { 
-        var newId = 0;
-
-        if ($scope.teams.length != 0) {
-            newId = $scope.teams[$scope.teams.length - 1].id + 1;
-        }
-
-        $scope.teams.push({ 
-            name: '',
-            id: newId
-        });
-    };
+            $scope.teams.push({ 
+                name: '',
+                id: newId
+            });
+        };
 
 
-    // LOCATIONS
-    $scope.locations = [ 
-        { name: "Baseball Field 1", id: 0 },
-        { name: "Baseball Field 2", id: 1 }];
+        // LOCATIONS
+        $scope.locations = [ 
+            { name: "Baseball Field 1", id: 0 },
+            { name: "Baseball Field 2", id: 1 }];
 
-    $scope.removeLocation = function(id) {
-        var oldLocations = $scope.locations;
-        $scope.locations = [];
-        angular.forEach(oldLocations, function(location) {
-            if(location.id != id) {
-                $scope.locations.push(location);
+        $scope.removeLocation = function(id) {
+            var oldLocations = $scope.locations;
+            $scope.locations = [];
+            angular.forEach(oldLocations, function(location) {
+                if(location.id != id) {
+                    $scope.locations.push(location);
+                }
+            });
+        };
+
+        $scope.addLocation = function() { 
+            var newId = 0;
+
+            if ($scope.locations.length != 0) {
+                newId = $scope.locations[$scope.locations.length - 1].id + 1;
             }
-        });
-    };
 
-    $scope.addLocation = function() { 
-        var newId = 0;
+            $scope.locations.push({ 
+                name: '',
+                id: newId
+            });
+        };
 
-        if ($scope.locations.length != 0) {
-            newId = $scope.locations[$scope.locations.length - 1].id + 1;
-        }
-
-        $scope.locations.push({ 
-            name: '',
-            id: newId
-        });
-    };
-
-    // START AND END DATES
-    $scope.startDate = null;
-    $scope.endDate = null;
+        // START AND END DATES
+        $scope.startDate = null;
+        $scope.endDate = null;
 
 
-});
+    });
+})();
